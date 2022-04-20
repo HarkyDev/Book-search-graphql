a1efd3a163909633e25a6f99cd16915f4be0c45a
Eoin Harkin <eharky@gmail.com>
2022-04-20 20:34:24 +0100

    login now working with queries and mutations

--- client/src/App.js
+++ client/src/App.js
@@ -6,44 +6,37 @@ import Navbar from "./components/Navbar";
 import { ApolloProvider } from "@apollo/react-hooks";
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-
 const client = new ApolloClient({
-  request: operation => {
-    const token = localStorage.getItem('id_token');
+  request: (operation) => {
+    const token = localStorage.getItem("id_token");
 
     operation.setContext({
       headers: {
-        authorization: token ? `Bearer ${token}` : ''
-      }
+        authorization: token ? `Bearer ${token}` : "",
+      },
     });
   },
-  uri: '/graphql',
+  uri: "/graphql",
   cache: new InMemoryCache(),
 });
 
 function App() {
   return (
     <ApolloProvider client={client}>
-    <Router>
-      <>
-        <Navbar />
-        <Routes>
-          <Route 
-            path='/' 
-            element={<SearchBooks />} 
-          />
-          <Route 
-            path='/saved' 
-            element={<SavedBooks />} 
-          />
-          <Route 
-            path='*'
-            element={<h1 className='display-2'>Wrong page!</h1>}
-          />
-        </Routes>
-      </>
-    </Router>
-    </ ApolloProvider>
+      <Router>
+        <>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<SearchBooks />} />
+            <Route path="/saved" element={<SavedBooks />} />
+            <Route
+              path="*"
+              element={<h1 className="display-2">Wrong page!</h1>}
+            />
+          </Routes>
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -3,7 +3,7 @@ import gql from 'graphql-tag';
 export const LOGIN_USER = gql`
     mutation login($email: String!, $password: String!) {
         login(email: $email, password: $password) {
-            token
+            signInKey
             user {
                 _id
                 username
@@ -16,7 +16,7 @@ export const LOGIN_USER = gql`
 export const ADD_USER = gql`
     mutation addUser($username:String!, $email:String!, $password: String!){
         addUser(username: $username, email:$email, password:$password) {
-            token
+            signInKey
             user {
                 _id
                 username
